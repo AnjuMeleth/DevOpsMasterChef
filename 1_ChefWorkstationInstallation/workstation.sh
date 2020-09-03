da75fdd0d541514560fb9a4c3546e001c8645cd2
AnjuMeleth <46922680+AnjuMeleth@users.noreply.github.com>
2020-09-03 11:55:22 +0530

    Update workstation.sh

--- 1_ChefWorkstationInstallation/workstation.sh
+++ 1_ChefWorkstationInstallation/workstation.sh
@@ -1,3 +1,3 @@
-sudo wget https://packages.chef.io/files/stable/chefworkstation/0.14/ubuntu/18.04/chefworkstation_0.14.16-1_amd64.deb 
-sudo dpkg -i chefworkstation_0.14.16-1_amd64.deb
+sudo wget https://packages.chef.io/files/stable/chefdk/4.10.0/ubuntu/18.04/chefdk_4.10.0-1_amd64.deb  
+sudo dpkg -i chefdk_4.10.0-1_amd64
 
